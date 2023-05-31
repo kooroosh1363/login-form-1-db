a8b65bfa60194d9d64949d2381cfff40037d842b
kooroosh1363 <peymanraad1363@gmail.com>
2023-05-31 23:45:17 +0400

    send into git hub files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>login-form</title>
+</head>
+
+<body>
+    <form action="login.php" method="POST">
+        <h2>LOGIN</h2>
+        <label for="">User Name</label>
+        <input type="text" name="u-name" placeholder="User Name">
+
+        <label for="">Password</label>
+        <input type="password" name="pass" placeholder="Password">
+
+        <div class="btn">
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</body>
+
+</html>

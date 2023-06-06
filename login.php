d84a5707d15a6e7e2a82d6d730556e38e1c1735f
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-06 18:26:21 +0400

    write php codes

--- login.php
+++ login.php
@@ -2,7 +2,19 @@
 
 
 
-echo "hello";
+if (isset($_POST['u_name']) && isset($_POST['pass'])) {
+    function validation($data){
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+ 
+    
+}else{
+    header('Location: index.php');
+    exit();
+}
 
 
 

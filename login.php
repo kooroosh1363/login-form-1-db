b56cee8dacdbf6aaa1e2cc55b9ddceff77a350d7
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-06 18:28:53 +0400

    write php codes 2

--- login.php
+++ login.php
@@ -9,7 +9,19 @@ if (isset($_POST['u_name']) && isset($_POST['pass'])) {
         $data = htmlspecialchars($data);
         return $data;
     }
- 
+    $u_name =validation($_POST['u_name']);
+    $pass =validation($_POST['pass']);
+
+    if (empty($u_name)) {
+        header('Location: index.php?error=User Name is required');
+        exit();
+    }else if(empty($pass)){
+        header('Location: index.php?error=Password is required');
+        exit();
+
+    }else{
+        echo "valid input";
+    }
     
 }else{
     header('Location: index.php');
